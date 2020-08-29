8870aea98f04961769d7375dab4e8675407b7f8b
Peter <pi.dudka@gmail.com>
2020-08-29 16:51:54 -0400

    Playing around with style for funsies

--- tutorial-app/src/Person/Person.js
+++ tutorial-app/src/Person/Person.js
@@ -7,6 +7,7 @@ const person = ( props ) => {
             <p>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
+            <button onClick={props.click}>Remove me!</button>
         </div>
     )
 };
